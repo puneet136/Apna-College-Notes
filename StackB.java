c1c95270888221885ff35bd99ea7c6313d35beb6
Puneet Agrawal <puneetagrawalspu@gmail.com>
2025-02-05 20:45:44 +0530

    Add stack file in master folder

---
+++ StackB.java
@@ -0,0 +1,56 @@
+import java.util.ArrayList;
+
+public class StackB {
+
+    static class Stack {
+
+        //Implementation Using ArrayList
+        static ArrayList<Integer> list = new ArrayList<>();
+
+        //Function to check stack is Empty or not
+        public static Boolean IsEmpty()
+        {
+            return list.isEmpty();                      //We can Write this thing "return list.size() == 0;"
+        }
+
+        //Function to add element(Push) in stack
+        public static void push(int data)
+        {
+            list.add(data);
+        }
+
+        //Function to delete Element (Pop) in stack
+        public static int pop()
+        {
+            //For Edge Cases
+            if (list.isEmpty())
+            {
+                return -1;
+            }
+            
+            int top = list.get(list.size() - 1);
+            list.remove(list.size() - 1);
+            return top;
+        }
+
+        //Function for show the peek element in stack
+        public static int peek() {
+            //For Edge Cases
+            if (list.isEmpty()) {
+                return -1;
+            }
+            return list.get(list.size() - 1);
+        }
+    }
+
+    public static void main(String[] args) {
+        Stack s = new Stack();
+        s.push(1);
+        s.push(2);
+        s.push(3);
+        while (!s.IsEmpty()) {
+            System.out.println(s.peek());
+            s.pop();
+        }
+    }
+}
