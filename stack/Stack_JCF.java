fbde98a0b456fdc39fda4ea39de1c4c513e024be
Puneet Agrawal <puneetagrawalspu@gmail.com>
2025-02-06 22:23:08 +0530

    Add Stack implementation using Framework and add push at bottom function in stack_JCF file

---
+++ stack/Stack_JCF.java
@@ -0,0 +1,29 @@
+package stack;
+import java.util.*;
+public class Stack_JCF {
+
+    //This Function is Implemented data in stack at bottom
+    public static void pushAtBottom(Stack<Integer> s , int data){
+
+        if(s.isEmpty()){
+            s.push(data);
+            return;
+        }
+        int top = s.pop();
+        pushAtBottom(s, data);
+        s.push(top);
+    }
+    public static void main(String[] args) {
+        Stack<Integer> s = new Stack<>();
+
+    s.push(1);
+    s.push(2);
+    s.push(3);
+
+    pushAtBottom(s , 4);
+    while(!s.isEmpty()){
+        System.out.println(s.peek());
+        s.pop();
+    }
+    }
+}
