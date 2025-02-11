76255718011e2e829576eb0b28ce5a4b37d9a484
Puneet Agrawal <puneetagrawalspu@gmail.com>
2025-02-11 21:59:11 +0530

    apna college

--- stack/Stock_Span.java
+++ stack/Stock_Span.java
@@ -29,7 +29,7 @@ public class Stock_Span {
         int span[] = new int [stock.length];
         stockSpan(stock, span);
         for(int i=0;i<span.length ; i++){
-            System.out.println(span[i] + " ");
+            System.out.print(span[i] + " ");
         }
     }
 }
