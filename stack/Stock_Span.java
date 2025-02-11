45424212eede6744abf2409270cd49ea7b4a09e8
Puneet Agrawal <puneetagrawalspu@gmail.com>
2025-02-11 18:30:52 +0530

    Add Stock Span problme in folder

---
+++ stack/Stock_Span.java
@@ -0,0 +1,35 @@
+package stack;
+import java.util.*;
+public class Stock_Span {
+
+    public static void stockSpan(int [] stock , int [] span){
+        Stack <Integer> s = new Stack<>();
+        s.push(0);
+        span[0] = 1;
+
+        for(int i= 1 ; i<stock.length ; i++){
+            int currPrice =stock[i];
+            while(!s.isEmpty() && currPrice >stock[s.peek()]){
+                s.pop();
+            }
+
+            if(s.isEmpty()){
+                span[i] = i+1;
+            }
+            else{
+                int prevHigh = s.peek();
+                span[i] = i - prevHigh;
+            }
+            s.push(i);
+        }
+    }
+    public static void main(String[] args) {
+        int stock [] = {100 , 80 , 60 , 70 , 60, 85, 100};
+
+        int span[] = new int [stock.length];
+        stockSpan(stock, span);
+        for(int i=0;i<span.length ; i++){
+            System.out.println(span[i] + " ");
+        }
+    }
+}
