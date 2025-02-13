665390d2fd1892857a9f7e6da4614d2767628489
Puneet Agrawal <puneetagrawalspu@gmail.com>
2025-02-13 13:32:34 +0530

    Add another approach to solve this problem

--- stack/Duplicate_paranthesis.java
+++ stack/Duplicate_paranthesis.java
@@ -3,34 +3,66 @@ package stack;
 import java.util.Stack;
 
 public class Duplicate_paranthesis{
-    public static boolean IsDuplicate(String str){
-        Stack<Character> s = new Stack<>();
 
-        for(int i =0;i<str.length() ; i++){
+        //Approach 1
+    // public static boolean IsDuplicate(String str){
+    //     Stack<Character> s = new Stack<>();
+
+    //     for(int i =0;i<str.length() ; i++){
+    //         char ch = str.charAt(i);
+
+    //         //Closing
+    //         if(ch == ')'){
+    //             int count = 0;
+    //             while(s.peek() != '('){
+    //                 s.pop();
+    //                 count++;
+    //             }
+    //             if(count <1){
+    //                 return true;    //Duplicate
+    //             }
+    //             else{
+    //                 s.pop();        //Opening Pair
+    //             }
+    //         }
+    //         else{
+    //             //Opening condition
+    //             s.push(ch);
+    //         }
+
+    //     }
+    //     return false;
+    // }
+
+            //Approach 2
+        
+        public static boolean IsDuplicate(String str){
+            Stack<Character> stack = new Stack<>();
+
+            for (int i=0;i<str.length() ;i++) {
             char ch = str.charAt(i);
+            if (ch == ')') {
+                // Check if the top of the stack is '(' (which means duplicate parentheses)
 
-            //Closing
-            if(ch == ')'){
-                int count = 0;
-                while(s.peek() != '('){
-                    s.pop();
-                    count++;
+                if (stack.peek() == '('){
+                    return true; // Found duplicate parentheses
                 }
-                if(count <1){
-                    return true;    //Duplicate
+                
+                // Pop elements until '(' is found
+                while (!stack.isEmpty() && stack.peek() != '(') {
+                    stack.pop();
                 }
-                else{
-                    s.pop();        //Opening Pair
-                }
-            }
-            else{
-                //Opening condition
-                s.push(ch);
-            }
 
+                // Pop the '('
+                stack.pop();
+            } else {
+                stack.push(ch); // Push all other characters
+            }
         }
-        return false;
-    }
+
+        return false; //
+        }
+        
     public static void main(String[] args) {
         //Valid String
         String str1 = "((a+b)+(c+d))";          //Output :- false
